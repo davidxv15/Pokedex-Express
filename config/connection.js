82259b2137ca07a5e6e171278a1ce015c60175d2
davidxv15 <davidxvaz@gmail.com>
2023-03-08 08:36:59 -0700

    MongoDB connected

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,21 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-connectionString = process.env.MONGO_DB_URI;
+const connectionString = process.env.MONGO_DB_URI;
 
 mongoose.connect(connectionString);
+
+//mongoDB successful connection message
+mongoose.connection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected... 🙌 🙌 🙌 `)
+})
+
+//mongoDB connection on error
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB connection error ⛔️ ⛔️ ⛔️ ', error)
+})
+
+//disconnect from mongoDB
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected ⚡️ 🔌 ⚡️')
+})
