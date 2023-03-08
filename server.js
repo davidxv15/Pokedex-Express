5d178da615efcd96a905d1449595aef694a2f2b2
davidxv15 <davidxvaz@gmail.com>
2023-03-07 20:15:00 -0700

    css start

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 
 const pokemonController = require('./controllers/pokemon');
 
+//this goes through the views dir, also the ejs
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 //my routes
 app.get('/', (req, res) => {
